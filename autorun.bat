0aa588954252f4a9135bb548e088fc0f7be02153
Prasanth S <prasanth.snk@gmail.com>
2024-06-09 07:16:37 +0530

    changes

--- autorun.bat
+++ autorun.bat
@@ -2,4 +2,6 @@ pip install requests
 
 cd scripts/
 
+python malayalam_m3u.py > ../malayalam_m3u.m3u
+
 python tamil_m3u.py > ../tamil_m3u.m3u
--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,8 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
+python3 malayalam_m3u.py > ../malayalam_m3u.m3u
+
 python3 tamil_m3u.py > ../tamil_m3u.m3u
 
 echo m3u grabbed
