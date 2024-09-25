b510aa07e64d30657f544562b8ef981a3aa29d66
Prasanth S <prasanth.snk@gmail.com>
2024-09-25 20:42:09 +0530

    changes

--- autorun.sh
+++ autorun.sh
@@ -10,6 +10,6 @@ python3 malayalam_m3u.py > ../malayalam_m3u.m3u
 python3 tamil_m3u.py > ../tamil_m3u.m3u
 python3 movies_m3u.py > ../movies_m3u.m3u
 python3 xxx_m3u.py > ../xxx_m3u.m3u
-python3 test_m3u.py > ../test_m3u.m3u
+# python3 test_m3u.py > ../test_m3u.m3u
 
 echo m3u grabbed
