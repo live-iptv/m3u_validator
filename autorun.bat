e744b79fe03673e24e8da1e92b7da04a6410ee2e
Prasanth S <prasanth.snk@gmail.com>
2025-10-05 10:57:35 +0530

    Comment out movies_m3u.py execution in autorun scripts
    Disabled the execution of movies_m3u.py in both autorun.bat and autorun.sh by commenting out the relevant lines. This change prevents movies_m3u.m3u from being generated during the autorun process.

--- autorun.bat
+++ autorun.bat
@@ -4,7 +4,7 @@ cd scripts/
 
 python malayalam_m3u.py > ../malayalam_m3u.m3u
 python tamil_m3u.py > ../tamil_m3u.m3u
-python movies_m3u.py > ../movies_m3u.m3u
+@REM python movies_m3u.py > ../movies_m3u.m3u
 python xxx_m3u.py > ../xxx_m3u.m3u
 python tamil_local_json.py > ../tamil_local_json.m3u
 python malayalam_local_json.py > ../malayalam_local_json.m3u
