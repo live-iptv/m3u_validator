43d793a071ff621d6405c1070516136e917b9b35
Prasanth S <prasanth.snk@gmail.com>
2025-10-05 11:15:01 +0530

    changes

--- autorun.sh
+++ autorun.sh
@@ -8,10 +8,10 @@ cd $(dirname $0)/scripts/
 
 python3 malayalam_m3u.py > ../malayalam_m3u.m3u
 python3 tamil_m3u.py > ../tamil_m3u.m3u
-# python3 movies_m3u.py > ../movies_m3u.m3u
-python3 xxx_m3u.py > ../xxx_m3u.m3u
 python3 tamil_local_json.py > ../tamil_local_json.m3u
 python3 malayalam_local_json.py > ../malayalam_local_json.m3u
+# python3 xxx_m3u.py > ../xxx_m3u.m3u
+# python3 movies_m3u.py > ../movies_m3u.m3u
 # python3 test_m3u.py > ../test_m3u.m3u
 
 echo m3u grabbed
